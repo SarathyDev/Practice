f46a29064e73447c506b79872614c8b5e5266202
Bala <Sarathy@BALAKRISHNAs-MacBook-Pro-2.local>
2016-09-04 21:17:51 -0700

    Text box Reset button

--- Practice.js
+++ Practice.js
@@ -1,2 +1,5 @@
 document.getElementsByClassName("date")[0].innerHTML = new Date().toDateString();
 document.getElementsByClassName("zoomDate")[0].innerHTML = new Date().toDateString();
+function changeText() {
+    document.getElementById("Comments").innerHTML = "";
+}
