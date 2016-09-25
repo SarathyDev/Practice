437597ce985b23d3d82ea24557b459ffea6429ac
Bala <Sarathy@BALAKRISHNAs-MacBook-Pro-2.local>
2016-09-25 12:21:10 -0700

    Adding AngularJS capabilities

--- Practice.js
+++ Practice.js
@@ -3,7 +3,10 @@ function changeText() {
     document.getElementById("Comments").innerHTML = "";
 }
 function capitalize() {
-    var name = document.getElementsByClassName("input_name");
+    var name = document.getElementsByClassName("inputName");
     name[0].value = name[0].value.toUpperCase();
     name[1].value = name[1].value.toUpperCase();
 }
+	var app = angular.module("angularApp", []);
+	app.directive("myDirective", function() { return { template: "<p>These are the items in the list!</p>" };});
+	app.controller("myControl", function($scope) { $scope.myName = names[0]; });
