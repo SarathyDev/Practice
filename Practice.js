2ba3f508215494397757e6b2c533ec899534d694
Bala <Sarathy@BALAKRISHNAs-MacBook-Pro-2.local>
2016-09-29 10:18:19 -0700

    Adding config file for JS and adding table.

--- Practice.js
+++ Practice.js
@@ -8,5 +8,30 @@ function capitalize() {
     name[1].value = name[1].value.toUpperCase();
 }
 	var app = angular.module("angularApp", []);
-	app.directive("myDirective", function() { return { template: "<p>These are the items in the list!</p>" };});
-	app.controller("myControl", function($scope) { $scope.myName = names[0]; });
+	app.directive("myDirective", function() { return { template: "<div>These are the names in the list = </div>" };});
+    app.controller("tableAngSort", function($scope) { 
+        $scope.database = [
+            {name:'Sarathy', rollNumber:9},
+            {name:'Arthi', rollNumber:6},
+            {name:'Gautham', rollNumber:17},
+            {name:'Edwin', rollNumber:16}
+        ]
+        $scope.orderByMethod = function (constraint) {$scope.selectedOrder=constraint;};
+    });
+    app.filter('customFormat', function() {
+        return function(x) {
+            var indexVariable, characs, mytxt = "";
+            for (indexVariable = 0; indexVariable < x.length; indexVariable++) {
+                characs = x[indexVariable];
+                if(indexVariable % 3 == 0) {
+                    characs = characs.toUpperCase();
+                }
+                mytxt += characs;
+            }
+            return mytxt;
+        };
+    });
+    app.controller('nameController', function($scope) {
+        $scope.nameList = ['Sarathy', 'Gautham', 'Arthi', 'Edwin']; 
+    });
+    
