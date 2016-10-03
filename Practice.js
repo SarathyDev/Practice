87b5a33e38824ebe2c36d6f43fff1fd9720bbcb1
Bala <Sarathy@BALAKRISHNAs-MacBook-Pro-2.local>
2016-10-03 12:57:54 -0700

    Added status code checker and 60 sec simple timer

--- Practice.js
+++ Practice.js
@@ -7,34 +7,46 @@ function capitalize() {
     name[0].value = name[0].value.toUpperCase();
     name[1].value = name[1].value.toUpperCase();
 }
-	var app = angular.module("angularApp", []);
-	app.directive("myDirective", function() { return { template: "<div>These are the names in the list = </div>" };});
-    app.controller("tableAngSort", function($scope) { 
-        $scope.database = [
-            {name:'Sarathy', rollNumber:9},
-            {name:'Arthi', rollNumber:6},
-            {name:'Gautham', rollNumber:17},
-            {name:'Edwin', rollNumber:16}
-        ]
-        $scope.orderByMethod = function (constraint) {$scope.selectedOrder=constraint;};
-    });
-    app.filter('customFormat', function() {
-        return function(x) {
-            var counter, characs, mytxt = "";
-            for (counter=0; counter<x.length; counter++) {
-                characs = x[counter];
-                if(counter % 3 == 0) {
-                    characs = characs.toUpperCase();
-                }
-                mytxt += characs;
+var app = angular.module("angularApp", []);
+app.directive("myDirective", function () { return { template: "<div>These are the names in the list = </div>" }; });
+app.controller("tableAngSort", function ($scope) {
+    $scope.database = [
+        { name: 'Sarathy', rollNumber: 9 },
+        { name: 'Arthi', rollNumber: 6 },
+        { name: 'Gautham', rollNumber: 17 },
+        { name: 'Edwin', rollNumber: 16 }
+    ]
+    $scope.orderByMethod = function (constraint) { $scope.selectedOrder = constraint; };
+});
+app.filter('customFormat', function () {
+    return function (x) {
+        var counter, characs, mytxt = "";
+        for (counter = 0; counter < x.length; counter++) {
+            characs = x[counter];
+            if (counter % 3 == 0) {
+                characs = characs.toUpperCase();
             }
-            return mytxt;
-        };
-    });
-    app.controller('nameController', function($scope) {
-        $scope.nameList = ['Sarathy', 'Gautham', 'Arthi', 'Edwin']; 
-    });
-    app.controller('headingTimeController', function($scope, $timeout) {
-        $scope.displayText = "HTML practice page";
-        $timeout(function() {$scope.displayText = "Welcome to my practice page"}, 2000);
-    });
+            mytxt += characs;
+        }
+        return mytxt;
+    };
+});
+app.controller('nameController', function ($scope) {
+    $scope.nameList = ['Sarathy', 'Gautham', 'Arthi', 'Edwin'];
+});
+app.controller('headingTimeController', function ($scope, $timeout) {
+    $scope.displayText = "HTML practice page";
+    $timeout(function () { $scope.displayText = "Welcome to my practice page" }, 2000);
+});
+app.controller('googleController', function ($scope, $http) {
+
+    $http.get("file:///Users/Sarathy/Documents/HTML/Practice.html")
+        .then(function (response) {
+            $scope.content = response.data;
+            $scope.statuscode = response.status;
+        });
+});
+app.controller('timerController', function ($scope, $interval) {
+    $scope.timer = 0;
+    $interval(function () { $scope.timer += 1; }, 1000);
+});
